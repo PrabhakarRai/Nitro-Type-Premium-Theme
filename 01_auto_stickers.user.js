5883430bc6204859fbc8b24afe3b718b839a03f7
Prabhakar Rai <student343.bhu@gmail.com>
2020-10-20 23:40:40 +0530

    Update 01_auto_stickers.user.js
    added update url

--- 01_auto_stickers.user.js
+++ 01_auto_stickers.user.js
@@ -5,5 +5,7 @@
 // @description  Part of Nitro Type Premium Theme - Posts Stickers Automatically in NT Race
 // @author       Prabhakar Rai
 // @match        https://www.nitrotype.com/race
+// @updateURL    https://github.com/PrabhakarRai/Nitro-Type-Premium-Theme/raw/main/01_auto_stickers.user.js
+// @downloadURL  https://github.com/PrabhakarRai/Nitro-Type-Premium-Theme/raw/main/01_auto_stickers.user.js
 // @grant        none
 // ==/UserScript==
