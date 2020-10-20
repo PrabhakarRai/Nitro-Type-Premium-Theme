39562eabc8252204ae6774b9c58dec3619d4a7a9
Prabhakar Rai <student343.bhu@gmail.com>
2020-10-20 23:39:07 +0530

    Create 01_auto_stickers.user.js

---
+++ 01_auto_stickers.user.js
@@ -0,0 +1,9 @@
+// ==UserScript==
+// @name         Auto Stickers
+// @namespace    https://www.theprabhakar.in/
+// @version      1.0
+// @description  Part of Nitro Type Premium Theme - Posts Stickers Automatically in NT Race
+// @author       Prabhakar Rai
+// @match        https://www.nitrotype.com/race
+// @grant        none
+// ==/UserScript==
