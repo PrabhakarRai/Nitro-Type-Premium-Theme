ff920a66c3a146cfae9adb11d60a9451e36fc3a5
Prabhakar Rai <student343.bhu@gmail.com>
2020-11-04 19:00:51 +0530

    Update 02_top3_badge.user.js

--- 02_top3_badge.user.js
+++ 02_top3_badge.user.js
@@ -1,1 +1,11 @@
-
+// ==UserScript==
+// @name         Top-3 Badge
+// @namespace    https://www.theprabhakar.in/
+// @version      1.0
+// @description  Part of Nitro Type Premium Series - Adds Top-3 Badge on Garage Page
+// @author       Prabhakar Rai
+// @match        https://www.nitrotype.com/garage
+// @updateURL    https://github.com/PrabhakarRai/Nitro-Type-Premium-Theme/raw/main/02_top3_badge.user.js
+// @downloadURL  https://github.com/PrabhakarRai/Nitro-Type-Premium-Theme/raw/main/02_top3_badge.user.js
+// @grant        none
+// ==/UserScript==
